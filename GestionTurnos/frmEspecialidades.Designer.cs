1246044ea073ae79fd7c2f4bf528e5ffc1e8d1d1
juancruzgauna <juancruzgauna97@gmail.com>
2024-10-17 20:01:26 -0300

    Se agregan botones para AMB Especialidad

--- GestionTurnos/frmEspecialidades.Designer.cs
+++ GestionTurnos/frmEspecialidades.Designer.cs
@@ -30,6 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEspecialidades));
             this.dgvEspecialidades = new System.Windows.Forms.DataGridView();
+            this.btnAgregarEspecialidad = new System.Windows.Forms.Button();
+            this.btnModificarEspecialidad = new System.Windows.Forms.Button();
+            this.btnEliminarEspecialidad = new System.Windows.Forms.Button();
+            this.btnSalirEspecialidad = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEspecialidades)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,11 +49,51 @@
             this.dgvEspecialidades.Size = new System.Drawing.Size(156, 150);
             this.dgvEspecialidades.TabIndex = 0;
             // 
+            // btnAgregarEspecialidad
+            // 
+            this.btnAgregarEspecialidad.Location = new System.Drawing.Point(58, 215);
+            this.btnAgregarEspecialidad.Name = "btnAgregarEspecialidad";
+            this.btnAgregarEspecialidad.Size = new System.Drawing.Size(62, 22);
+            this.btnAgregarEspecialidad.TabIndex = 1;
+            this.btnAgregarEspecialidad.Text = "Agregar";
+            this.btnAgregarEspecialidad.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarEspecialidad
+            // 
+            this.btnModificarEspecialidad.Location = new System.Drawing.Point(146, 215);
+            this.btnModificarEspecialidad.Name = "btnModificarEspecialidad";
+            this.btnModificarEspecialidad.Size = new System.Drawing.Size(62, 23);
+            this.btnModificarEspecialidad.TabIndex = 2;
+            this.btnModificarEspecialidad.Text = "Modificar";
+            this.btnModificarEspecialidad.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarEspecialidad
+            // 
+            this.btnEliminarEspecialidad.Location = new System.Drawing.Point(226, 215);
+            this.btnEliminarEspecialidad.Name = "btnEliminarEspecialidad";
+            this.btnEliminarEspecialidad.Size = new System.Drawing.Size(62, 23);
+            this.btnEliminarEspecialidad.TabIndex = 3;
+            this.btnEliminarEspecialidad.Text = "Eliminar";
+            this.btnEliminarEspecialidad.UseVisualStyleBackColor = true;
+            // 
+            // btnSalirEspecialidad
+            // 
+            this.btnSalirEspecialidad.Location = new System.Drawing.Point(273, 258);
+            this.btnSalirEspecialidad.Name = "btnSalirEspecialidad";
+            this.btnSalirEspecialidad.Size = new System.Drawing.Size(62, 23);
+            this.btnSalirEspecialidad.TabIndex = 4;
+            this.btnSalirEspecialidad.Text = "Salir";
+            this.btnSalirEspecialidad.UseVisualStyleBackColor = true;
+            // 
             // frmEspecialidades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(356, 293);
+            this.Controls.Add(this.btnSalirEspecialidad);
+            this.Controls.Add(this.btnEliminarEspecialidad);
+            this.Controls.Add(this.btnModificarEspecialidad);
+            this.Controls.Add(this.btnAgregarEspecialidad);
             this.Controls.Add(this.dgvEspecialidades);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmEspecialidades";
@@ -64,5 +108,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvEspecialidades;
+        private System.Windows.Forms.Button btnAgregarEspecialidad;
+        private System.Windows.Forms.Button btnModificarEspecialidad;
+        private System.Windows.Forms.Button btnEliminarEspecialidad;
+        private System.Windows.Forms.Button btnSalirEspecialidad;
     }
 }
